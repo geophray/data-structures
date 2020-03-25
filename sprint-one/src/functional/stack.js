8ef72756a909ee87663b6700526ca25c4d51c686
gen0pp <mattg2@gmail.com>
2020-03-25 15:23:48 -0700

    First commit, added stack functions (functional)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
+
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var keys = Object.keys(storage);
+
+  //var theSize = Object.size(storage);
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var indexToRemove = someInstance.size();
+    indexToRemove--;
+    var toRemove = storage[indexToRemove];
+    delete storage[indexToRemove];
+    return toRemove;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
