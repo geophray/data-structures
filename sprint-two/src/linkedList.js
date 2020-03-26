a3a3d69588fe885c4be977c1c9f3f608553a99fd
gen0pp <mattg2@gmail.com>
2020-03-26 16:51:12 -0700

    Completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,18 +4,57 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    var newNode = new Node(value);
+
+    if (this.tail !== null) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    if (this.tail === null) {
+      this.tail = newNode;
+      this.head = newNode;
+    }
+
   };
 
   list.removeHead = function() {
+    var tempHead = this.head.value;
+    this.head = this.head.next;
+    return tempHead;
   };
 
   list.contains = function(target) {
+
+    if (this.head.next === null) return false;
+    if (this.head.value === target) return true;
+
+    var currentNode = this.head;
+
+    while (currentNode !== null)
+    {
+      if (currentNode.value === target){
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+
+
+    // var nextNode = this.head.next;
+    // if (currentNode.next.value === target) {
+    //   return true;
+    // } else {
+    //   // currentNode.next
+    //   return nextNode.contains(target);
+    // }
+
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function(value, ) {
   var node = {};
 
   node.value = value;
