ac225e1c521edbe8835429e4e8ca6c16ddeae195
gen0pp <mattg2@gmail.com>
2020-03-28 12:32:57 -0700

    Added to binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,55 @@
 var BinarySearchTree = function(value) {
+
+  var bst = Object.create(stackMethods);
+  bst.value = value;
+  bst.left = undefined; // less than
+  bst.right = undefined; // greather than
+  return bst;
+
 };
 
+
+var stackMethods = {};
+
+stackMethods.insert = function(value) {
+
+  if (this.value > value) {
+    if (this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+  if (this.value < value) {
+    if (this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+stackMethods.contains = function(value) {
+
+  if (this.value === value) {
+    return true;
+  } else if (this.value < value && this.value.right !== undefined) {
+    return this.right.contains(value);
+  } else if (this.value > value && this.value.left !== undefined) {
+    return this.left.contains(value);
+  } else {
+    return false;
+  }
+
+
+
+};
+
+stackMethods.depthFirstLog = function(value) {
+
+
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,49 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (!this._storage[index]) {
+    this._storage[index] = [];
+  }
+
+  var bucket = this._storage[index];
+
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (bucket[i][0] === k) {
+      bucket[i][1] = v;
+    }
+  }
+
+  bucket.push([k, v]);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+
+  this._storage[index] = _.reject(bucket, function sort(element) {
+
+    if (element[0] === k) {
+      return true;
+    }
+
+    return false;
+
+  });
+
 };
 
 
